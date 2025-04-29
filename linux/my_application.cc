e5ad0ffea950e32ef157fd7841d14d3b10522252
brachy84 <brachy84@gmail.com>
2025-04-29 17:29:48 +0200

    fix rename

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "brachys_armor_set_searcher");
+    gtk_header_bar_set_title(header_bar, "brachydium_searcher");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "brachys_armor_set_searcher");
+    gtk_window_set_title(window, "brachydium_searcher");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
